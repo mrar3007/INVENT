63c180ec20439daaa796018716a495319fae6ffd
Arkadiy Petrov <arkadiy271020@gmail.com>
2022-06-26 16:33:50 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:untitled3/domain/user.dart';
+import 'package:untitled3/services/landing.dart';
+import 'package:untitled3/services/auth.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<MyUser?>.value( 
+      initialData: null,
+      value: AuthService().currentUser,
+      child: MaterialApp(
+        title: 'INVENT',
+        home: LandingPage(),
+      ),
+    );
+  }
+}
